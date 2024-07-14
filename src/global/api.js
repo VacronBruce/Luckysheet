7c0f33b6b9f84d7b6f01da21578634aa53bc3dcd
Bruce <homan311@gmail.com>
2024-07-14 09:29:11 -0400

    feat: add luckysheet import

--- src/global/api.js
+++ src/global/api.js
@@ -42,6 +42,7 @@ import dayjs from "dayjs";
 import {getRangetxt } from '../methods/get';
 import {luckysheetupdateCell} from '../controllers/updateCell';
 import luckysheetSearchReplace from "../controllers/searchReplace";
+import { importXlsxUrl} from '../expendPlugins/importXlsx/plugin';
 
 const IDCardReg = /^\d{6}(18|19|20)?\d{2}(0[1-9]|1[12])(0[1-9]|[12]\d|3[01])\d{3}(\d|X)$/i;
 
@@ -6948,4 +6949,8 @@ export function openSearchDialog(source = 1){
     luckysheetSearchReplace.createDialog(source);
     luckysheetSearchReplace.init();
     $("#luckysheet-search-replace #searchInput input").focus();
+}
+
+export function importXlsxForSheet(url, name) {
+    importXlsxUrl(url, name);
 }
